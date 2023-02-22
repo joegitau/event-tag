db2e4cbf9e65d2f6d45bb6d02d40ac61f91fa71f
Joseph Gitau <joseph.gitau@canter.fi>
2023-02-22 22:51:21 +0200

    Implement AttendeeService to provide abstaction layer btwn DAOs & app's biz logic

---
+++ src/main/scala/com/joegitau/services/AttendeeService.scala
@@ -0,0 +1,35 @@
+package com.joegitau.services
+
+import com.joegitau.model.{Attendee, PatchAttendee}
+import com.joegitau.slick.dao.attendee.AttendeeDao
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait AttendeeService {
+  def createAttendee(attendee: Attendee): Future[Attendee]
+  def getAttendeeById(id: Long): Future[Option[Attendee]]
+  def getAttendeeByLastName(lastName: String): Future[Option[Attendee]]
+  def getAllAttendees: Future[Seq[Attendee]]
+  def updateAttendee(id: Long, attendee: PatchAttendee): Future[Option[Attendee]]
+  def deleteAttendee(id: Long): Future[String]
+}
+
+class AttendeeServiceImpl(attendeeDao: AttendeeDao)(implicit ec: ExecutionContext) extends AttendeeService {
+  override def createAttendee(attendee: Attendee): Future[Attendee] =
+    attendeeDao.createAttendee(attendee)
+
+  override def getAttendeeById(id: Long): Future[Option[Attendee]] =
+    attendeeDao.getAttendeeById(id)
+
+  override def getAttendeeByLastName(lastName: String): Future[Option[Attendee]] =
+    attendeeDao.getAttendeeByLastName(lastName)
+
+  override def getAllAttendees: Future[Seq[Attendee]] =
+    attendeeDao.getAllAttendees
+
+  override def updateAttendee(id: Long, attendee: PatchAttendee): Future[Option[Attendee]] =
+    attendeeDao.updateAttendee(id, attendee)
+
+  override def deleteAttendee(id: Long): Future[String] =
+    attendeeDao.deleteAttendee(id)
+}
