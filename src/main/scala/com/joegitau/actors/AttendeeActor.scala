4817ebc2df1b1c90b8123e158001fa5eff3c8b4d
Joseph Gitau <joseph.gitau@canter.fi>
2023-02-23 11:35:23 +0200

    Implement Attendee actor + related commands

---
+++ src/main/scala/com/joegitau/actors/AttendeeActor.scala
@@ -0,0 +1,56 @@
+package com.joegitau.actors
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import akka.pattern.StatusReply
+import akka.pattern.StatusReply.ErrorMessage
+import com.joegitau.protocol.AttendeeProtocol.AttendeeCommand
+import com.joegitau.protocol.AttendeeProtocol.AttendeeResponse._
+import com.joegitau.services.AttendeeService
+
+import scala.util.{Failure, Success}
+
+object AttendeeActor {
+  def apply(attendeeService: AttendeeService): Behavior[AttendeeCommand] = Behaviors.receive { (ctx, msg) =>
+    implicit val ec = ctx.executionContext
+
+    ctx.log.info("::: Attendee actor started. :::")
+
+    msg match {
+      case AttendeeCommand.createAttendee(attendee, replyTo)       =>
+        attendeeService.createAttendee(attendee).onComplete {
+          case Success(attendee) => replyTo ! StatusReply.success(CreateAttendeeRsp(attendee))
+          case Failure(ex)       => replyTo ! StatusReply.error(ErrorMessage(s"Failed to create attendee. ${ex.getMessage}"))
+        }
+        Behaviors.same
+
+      case AttendeeCommand.GetAttendee(id, replyTo)                =>
+        attendeeService.getAttendeeById(id).onComplete {
+          case Success(maybeAttendee) => replyTo ! StatusReply.success(GetAttendeeRsp(maybeAttendee))
+          case Failure(ex)            => replyTo ! StatusReply.error(ErrorMessage(s"Failed to fetch attendee with id: $id. ${ex.getMessage}"))
+        }
+        Behaviors.same
+
+      case AttendeeCommand.GetAttendees(replyTo)                   =>
+        attendeeService.getAllAttendees.onComplete {
+          case Success(attendees) => StatusReply.success(GetAttendeesRsp(attendees.toList))
+          case Failure(ex)        => replyTo ! StatusReply.error(ErrorMessage(s"Failed to fetch attendees. ${ex.getMessage}"))
+        }
+        Behaviors.same
+
+      case AttendeeCommand.UpdateAttendee(id, attendee, replyTo)   =>
+        attendeeService.updateAttendee(id, attendee).onComplete {
+          case Success(updatedAttendee) => replyTo ! StatusReply.success(UpdateAttendeeRsp(updatedAttendee))
+          case Failure(ex)              => replyTo ! StatusReply.error(ErrorMessage(s"Failed to update attendee with id: $id. ${ex.getMessage}"))
+        }
+        Behaviors.same
+
+      case AttendeeCommand.DeleteAttendee(id, replyTo)             =>
+        attendeeService.deleteAttendee(id).onComplete {
+          case Success(_)  => replyTo ! StatusReply.success(DeleteAttendeeRsp(id))
+          case Failure(ex) => replyTo ! StatusReply.error(ErrorMessage(s"Failed to delete attendee with id: $id. ${ex.getMessage}"))
+        }
+        Behaviors.same
+    }
+  }
+}
---
+++ src/main/scala/com/joegitau/protocol/AttendeeProtocol.scala
@@ -0,0 +1,26 @@
+package com.joegitau.protocol
+
+import akka.actor.typed.ActorRef
+import akka.pattern.StatusReply
+import com.joegitau.model.{Attendee, PatchAttendee}
+import com.joegitau.serialization.CborSerializable
+
+object AttendeeProtocol {
+  sealed trait AttendeeCommand extends CborSerializable
+  object AttendeeCommand {
+    case class createAttendee(attendee: Attendee, replyTo: ActorRef[StatusReply[AttendeeResponse]])                extends AttendeeCommand
+    case class GetAttendee(id: Long, replyTo: ActorRef[StatusReply[AttendeeResponse]])                             extends AttendeeCommand
+    case class GetAttendees(replyTo: ActorRef[StatusReply[AttendeeResponse]])                                      extends AttendeeCommand
+    case class UpdateAttendee(id: Long, attendee: PatchAttendee, replyTo: ActorRef[StatusReply[AttendeeResponse]]) extends AttendeeCommand
+    case class DeleteAttendee(id: Long, replyTo: ActorRef[StatusReply[AttendeeResponse]])                          extends AttendeeCommand
+  }
+
+  sealed trait AttendeeResponse extends CborSerializable
+  object AttendeeResponse {
+    case class CreateAttendeeRsp(attendee: Attendee)                extends AttendeeResponse
+    case class GetAttendeeRsp(maybeAttendee: Option[Attendee])      extends AttendeeResponse
+    case class GetAttendeesRsp(attendees: List[Attendee])           extends AttendeeResponse
+    case class UpdateAttendeeRsp(updatedAttendee: Option[Attendee]) extends AttendeeResponse
+    case class DeleteAttendeeRsp(id: Long)                          extends AttendeeResponse
+  }
+}
